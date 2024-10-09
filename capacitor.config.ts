9ff1e85189f52de4eac97db9d25f15cf328dcf17
Geoffrey026 <geoffreymatantu026@student.unsrat.ac.id>
2024-10-09 17:20:21 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'weathermap',
+  webDir: 'www'
+};
+
+export default config;
